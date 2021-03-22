283518269045add7928ae85a9efebbd16e4bfe68
itay taasa <itaytaasa@gmail.com>
2021-03-22 18:39:05 +0200

    test

--- src/config/environment/index.js
+++ src/config/environment/index.js
@@ -2,6 +2,6 @@ const development = require('./development')
 const production = require('./production')
 let environment = development;
 if (process.env.NODE_ENV === 'production') {
-    enviroment = production
+    environment = production
 }
 module.exports = environment;
--- src/config/environment/production.js
+++ src/config/environment/production.js
@@ -5,3 +5,5 @@ port: process.env.PORT,
 dbUrl : process.env.DB_URL
 
 };
+
+//  mongodb+srv://ItayTaasa:DrorTaasa@cluster0.u4v4p.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
